79e265b2b0a2d4c862e22803feb7c997dcb4a9db
georgenicole <georgenicole1402@gmail.com>
2022-07-20 09:46:11 -0300

    it has created the subtitle

---
+++ src/components/Subtitle/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles.css"
+
+const Subtitle = () => {
+  return (
+    <h5 className='subtitle-todoList'>¿Qué cosas tenés que terminar hoy?</h5>
+  )
+}
+
+export default Subtitle
