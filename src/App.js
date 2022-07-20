3f4438723865fb98ef4a27311237374ebbb60cfc
georgenicole <georgenicole1402@gmail.com>
2022-07-20 09:47:21 -0300

    it has injected : - the title component -the subtitle component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import "./App.css";
 import Layout from "./components/Layout";
+import Subtitle from "./components/Subtitle";
+import Title from "./components/Title";
 
 function App() {
   return (
     <Layout>
-      
+      <Title/>
+      <Subtitle/>
     </Layout>
   );
 }
