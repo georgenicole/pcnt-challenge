4bc30fb5b0d2a9b907588b0e3d6b70c6979d8293
georgenicole <georgenicole1402@gmail.com>
2022-07-21 10:17:19 -0300

    it has created the to Do list component

---
+++ src/components/ToDoList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const ToDoList = () => {
+  return (
+    <ul className="Container-ToDoList">
+      <p className="title-ToDoList ">To do List</p>
+    </ul>
+  );
+};
+
+export default ToDoList;
