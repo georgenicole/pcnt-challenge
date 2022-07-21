8c8443f1f9b0009d30780708837548d9ef3c776b
georgenicole <georgenicole1402@gmail.com>
2022-07-21 11:33:35 -0300

    it has injected the ToDoList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Layout from "./components/Layout";
@@ -7,12 +7,14 @@ import Title from "./components/Title";
 import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [list, setList] = useState([{ title: "hola, ya esta listo", checked: true }]);
+
   return (
     <Layout>
-      <Title/>
-      <Subtitle/>
-      <Input/>
-      <ToDoList/>
+      <Title />
+      <Subtitle />
+      <Input />
+      <ToDoList data={list} />
     </Layout>
   );
 }
