bb6023d5f4800fe698c3d3868f3efbd33e986c77
georgenicole <georgenicole1402@gmail.com>
2022-07-23 13:36:11 -0300

    REFACTOR: it has added the custom image checked

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,11 +1,20 @@
 import "./styles.css";
+import Check from "../../assets/icons/checked.jpg";
+import UnCheck from "../../assets/icons/unchecked.jpg";
 
 const Item = ({ title, checked, onChange }) => {
 
+  
+
   return (
     <li className="item">
-      <input type="checkbox" checked={checked} onChange={onChange} />
-      <span className="title-span">{title}</span>
+      <label>
+        <input type="checkbox" checked={checked} onChange={onChange} />
+        <img  src={checked ? Check : UnCheck}
+        alt={`${checked ? "selected" : "non-selected"}`}/>
+        <span className="title-span">{title}</span>
+
+      </label>
     </li>
   );
 };
