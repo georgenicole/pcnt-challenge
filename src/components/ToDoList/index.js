1fbbfeea73334d0b2db9fa52b5afc76ccc03fb4d
georgenicole <georgenicole1402@gmail.com>
2022-07-23 13:33:54 -0300

    it has added the button Add in TodoList

--- src/components/ToDoList/index.js
+++ src/components/ToDoList/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Item from "../Item";
+import iconPlusButton from "../../assets/icons/icon-plus-button.svg";
 import "./styles.css";
 
 const ToDoList = ({ data }) => {
   return (
     <div className="Container-ToDoList">
       <p className="title-ToDoList ">To do List</p>
+      <input
+        type="image"
+        id="image"
+        alt="btnplus"
+        src={iconPlusButton}
+        className="btn-plus"
+      ></input>
       <ul className="list">
         {data.map((value) => (
           <Item
