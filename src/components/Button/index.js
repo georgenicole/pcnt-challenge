23f6f032d5a839cc19de8f5b8363baccb7255c9f
georgenicole <georgenicole1402@gmail.com>
2022-07-23 13:31:20 -0300

    it has created the Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Button = () => {
+  return (
+    <div style={{ marginTop: 180, padding: 0 }}>
+      <button className="btn-add">Agregar</button>
+    </div>
+  );
+};
+
+export default Button;
