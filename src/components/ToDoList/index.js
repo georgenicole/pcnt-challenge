0625af6aa07330554d9b0a8972f765e6bee7f93a
georgenicole <georgenicole1402@gmail.com>
2022-07-25 03:32:58 -0300

    REFACTOR: ToDolist has modified

--- src/components/ToDoList/index.js
+++ src/components/ToDoList/index.js
@@ -1,29 +1,68 @@
-import React from "react";
 import Item from "../Item";
 import iconPlusButton from "../../assets/icons/icon-plus-button.svg";
+import spand from "../../assets/icons/spand.svg";
+import Card from "../Card";
+import Tooltip from "../Tooltip";
+import { useEffect, useState } from "react";
 import "./styles.css";
+import { states } from "../../utils";
 
-const ToDoList = ({ data }) => {
+
+const ToDoList = ({
+  data,
+  options,
+  optionSelected,
+  onCheck,
+  onFilter,
+  showTooltip,
+  onShowFilter,
+  onNewList
+}) => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
+  const handleFilter = (option) => {
+    onFilter(option);
+
+    if (option.state === states.all) {
+      setList(data);
+    } else {
+      const values = data.filter((el) => el.state === option.state);
+      setList(values);
+    }
+  };
   return (
-    <div className="Container-ToDoList">
-      <p className="title-ToDoList ">To do List</p>
-      <input
-        type="image"
-        id="image"
-        alt="btnplus"
-        src={iconPlusButton}
-        className="btn-plus"
-      ></input>
-      <ul className="list">
-        {data.map((value) => (
-          <Item
-            checked={value.checked}
-            onChange={() => {}}
-            title={value.title}
+    <Card className="card-todos">
+      <Tooltip
+        onClick={handleFilter}
+        options={options}
+        show={showTooltip}
+        optionActive={optionSelected}
+      />
+      <div className="row wrapper-title">
+        <div className="row">
+          <h2 className="title-todo-list">To do List</h2>
+          <img
+            src={iconPlusButton}
+            alt="add new list todo"
+            className="btn-plus"
+            onClick={onNewList}
           />
+        </div>
+        <button className="btn-spand" onClick={onShowFilter}>
+          <span>{optionSelected.name}</span>
+          <img src={spand} alt="spand" />
+        </button>
+      </div>
+      <ul className="list">
+        {list.map((item) => (
+          <Item key={item.id} item={item} onClick={() => onCheck(item)} />
         ))}
       </ul>
-    </div>
+    </Card>
   );
 };
 
