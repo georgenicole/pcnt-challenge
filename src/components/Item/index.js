18735b0af40c9c8a86d7a31051c25de214c75cf7
georgenicole <georgenicole1402@gmail.com>
2022-07-25 03:37:03 -0300

    REFACTOR: Item has modified and his styles too

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,19 +2,18 @@ import "./styles.css";
 import Check from "../../assets/icons/checked.jpg";
 import UnCheck from "../../assets/icons/unchecked.jpg";
 
-const Item = ({ title, checked, onChange }) => {
-
-  
-
+const Item = ({ item, onClick }) => {
+  const { checked, title } = item;
   return (
-    <li className="item">
-      <label>
-        <input type="checkbox" checked={checked} onChange={onChange} />
-        <img  src={checked ? Check : UnCheck}
-        alt={`${checked ? "selected" : "non-selected"}`}/>
-        <span className="title-span">{title}</span>
-
-      </label>
+    <li className={`row item ${checked ? "checked" : ""}`} onClick={onClick}>
+      <div className="row">
+        <img
+          src={checked ? Check : UnCheck}
+          alt={`${checked ? "selected" : "non-selected"}`}
+          className="img-check"
+        />
+        <span className="title-todo">{title}</span>
+      </div>
     </li>
   );
 };
